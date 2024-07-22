f7b5b12390b4dc26d5dda95d201ef9cd908a1454
kevthedev100 <136115811+kevthedev100@users.noreply.github.com>
2024-07-22 20:57:53 +0200

    fixed user

--- lib/subscription.ts
+++ lib/subscription.ts
@@ -2,35 +2,29 @@ import { auth } from "@clerk/nextjs";
 
 import prismadb from "@/lib/prismadb";
 
-const DAY_IN_MS = 86_400_000;
+const DAY_IN_MS = 86_400_00;
 
 export const checkSubscription = async () => {
   const { userId } = auth();
 
-  if (!userId) {
-    return true;
-  }
+  if (!userId) return false;
 
   const userSubscription = await prismadb.userSubscription.findUnique({
-    where: {
-      userId: userId,
-    },
+    where: { userId },
     select: {
       stripeSubscriptionId: true,
       stripeCurrentPeriodEnd: true,
       stripeCustomerId: true,
-      stripePriceId: true,
-    },
-  })
+      stripePriceId: true
+    }
+  });
 
-  if (!userSubscription) {
-    return true;
-  }
+  if (!userSubscription) return false;
 
   const isValid =
     userSubscription.stripePriceId &&
-    userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now()
+    userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS >
+      Date.now();
 
   return !!isValid;
-  
 };

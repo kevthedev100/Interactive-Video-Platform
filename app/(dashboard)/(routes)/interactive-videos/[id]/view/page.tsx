16a9c189e283152950face1a362ad828713bb044
kevthedev100 <136115811+kevthedev100@users.noreply.github.com>
2024-08-04 03:16:36 +0200

    view - back

--- app/(dashboard)/(routes)/interactive-videos/[id]/view/page.tsx
+++ app/(dashboard)/(routes)/interactive-videos/[id]/view/page.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect, useRef } from "react";
 import { useParams } from "next/navigation";
 import * as lucideIcons from "lucide-react";
 
-
 declare global {
   interface Document {
     mozFullScreenElement?: Element;
@@ -99,12 +98,7 @@ const ViewInteractiveVideo = () => {
   };
 
   if (loading) {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen bg-black">
-        <img src="/Videyou-Logo.png" alt="Videyou Logo" className="w-1/2 max-w-xs" />
-        <p className="text-white mt-6">Interaktive Videos von Videyou</p>
-      </div>
-    );
+    return <div className="text-white">Loading...</div>;
   }
 
   return (
